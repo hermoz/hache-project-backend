70f55bbe16277f3967bffe333a7df94c78ee0781
hermoz <herminiamozomoreno@gmail.com>
2020-04-06 19:55:17 +0200

    PrivilegeDto, RoleDto y UserDto
    Definición de DTO para usarlos como transferencia entre cliente-servidor (entidades para mapeo)

---
+++ src/main/java/hmm/architecturestudio/management/dto/PrivilegeDto.java
@@ -0,0 +1,25 @@
+package hmm.architecturestudio.management.dto;
+
+public class PrivilegeDto {
+
+	private Long id;
+	private String name;
+	
+	public PrivilegeDto() {
+    }
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ src/main/java/hmm/architecturestudio/management/dto/RoleDto.java
@@ -0,0 +1,23 @@
+package hmm.architecturestudio.management.dto;
+
+public class RoleDto {
+	
+	private Long id;
+    private String name;
+    
+    public RoleDto() {
+    }
+    
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
