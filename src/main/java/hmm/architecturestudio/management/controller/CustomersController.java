5155cc2002476ea778e2c69aab49167cdb1acf0d
hermoz <herminiamozomoreno@gmail.com>
2020-04-29 19:57:16 +0200

    CustomersController - createCustomer endpoint

--- src/main/java/hmm/architecturestudio/management/controller/CustomersController.java
+++ src/main/java/hmm/architecturestudio/management/controller/CustomersController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,13 +13,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import hmm.architecturestudio.management.dto.CustomerDto;
 import hmm.architecturestudio.management.exception.PrivilegesException;
+import hmm.architecturestudio.management.exception.ValidationServiceException;
 import hmm.architecturestudio.management.model.Customer;
 import hmm.architecturestudio.management.service.CustomersService;
 
@@ -45,8 +51,23 @@ public class CustomersController {
 	    return convertToDto(customer.get());
 	}
     
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public CustomerDto createCustomer(@Valid @RequestBody CustomerDto customerDto) throws Exception {
+        Customer customer = convertToEntity(customerDto);
+        Customer createdCustomer = customersService.createCustomer(customer);
+
+        return convertToDto(createdCustomer);
+    }
+    
     private CustomerDto convertToDto(Customer customer) {
         CustomerDto customerDto = modelMapper.map(customer, CustomerDto.class);
         return customerDto;
+    }
+    
+    private Customer convertToEntity(CustomerDto customerDto) {
+        Customer customer = modelMapper.map(customerDto, Customer.class);
+        return customer;
     }
 }
