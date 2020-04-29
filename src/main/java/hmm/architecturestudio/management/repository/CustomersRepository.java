fd698bf615475ea3365a3f75aeca417044e47c02
hermoz <herminiamozomoreno@gmail.com>
2020-04-29 19:30:05 +0200

    CustomersRepository - añadimos métodos de búsqueda

--- src/main/java/hmm/architecturestudio/management/repository/CustomersRepository.java
+++ src/main/java/hmm/architecturestudio/management/repository/CustomersRepository.java
@@ -1,8 +1,22 @@
 package hmm.architecturestudio.management.repository;
 
 import hmm.architecturestudio.management.model.Customer;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CustomersRepository extends JpaRepository<Customer, Long> {
-	
+
+    @Query("SELECT c FROM Customer c WHERE c.cif = :cif")
+    public Optional<Customer> findByCif(@Param("cif") String cif);
+
+    @Query("SELECT c FROM Customer c WHERE c.email = :email")
+    public Optional<Customer> findByEmail(@Param("email") String email);
+
+    @Query("SELECT c FROM Customer c WHERE c.phone = :phone")
+    public Optional<Customer> findByPhone(@Param("phone") String phone);
+    
 }
