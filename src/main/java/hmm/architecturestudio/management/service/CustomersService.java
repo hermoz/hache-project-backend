5a91d2d2b286c1894df74e3a3f3dc3aab86ac61d
hermoz <herminiamozomoreno@gmail.com>
2020-04-29 20:05:25 +0200

    CustomersService - updateCustomer

--- src/main/java/hmm/architecturestudio/management/service/CustomersService.java
+++ src/main/java/hmm/architecturestudio/management/service/CustomersService.java
@@ -89,5 +89,53 @@ public class CustomersService {
         // Save customer
         return this.customersRepository.save(customer);
     }
+    
+    /*
+     * Update User
+     */
+    
+    public Customer updateCustomer(Customer customer) throws PrivilegesException, ValidationServiceException {
+
+        if (!privilegesChecker.hasPrivilege("UPDATE_CUSTOMERS",
+                SecurityContextHolder.getContext().getAuthentication().getAuthorities())
+        )
+        {
+            throw new PrivilegesException("UPDATE_CUSTOMERS");
+        }
+
+        Optional<Customer> optDestinationCustomer = customersRepository.findById(customer.getId());
+        Customer destinationCustomer = optDestinationCustomer.get();
+
+        // Check unique fields (like cif, email, phone) not exits
+
+        if (customersRepository.findByCif(customer.getCif()).isPresent()) {
+            throw new ValidationServiceException("Cif in use");
+        }
+
+        if (customersRepository.findByEmail(customer.getEmail()).isPresent()) {
+            throw new ValidationServiceException("Email in use");
+        }
+
+        if (customersRepository.findByPhone(customer.getPhone()).isPresent()) {
+            throw new ValidationServiceException("Phone in use");
+        }
+
+        // We search the projects
+        Set<Long> projectsIds = customer.getProjects().stream().map(p -> p.getId()).collect(Collectors.toSet());
+        List<Project> projects = projectsRepository.findAllById(projectsIds);
+
+        // We update the fields
+        destinationCustomer.setName(customer.getName());
+        destinationCustomer.setCif(customer.getCif());
+        destinationCustomer.setTaxResidence(customer.getTaxResidence());
+        destinationCustomer.setPhone(customer.getPhone());
+        destinationCustomer.setPopulation(customer.getPopulation());
+        destinationCustomer.setEmail(customer.getEmail());
+
+        customer.setProjects(projects.stream().collect(Collectors.toSet()));
+
+        // Save customer
+        return this.customersRepository.save(destinationCustomer);
+    }
 
 }
