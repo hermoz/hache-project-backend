6f9b55ec206f0f9502fa0d6e8bfd1f317429eae3
hermoz <herminiamozomoreno@gmail.com>
2020-04-29 19:02:10 +0200

    Customers Service - findAll
    Creación del servicio con el método de búsqueda general

---
+++ src/main/java/hmm/architecturestudio/management/service/CustomersService.java
@@ -0,0 +1,34 @@
+package hmm.architecturestudio.management.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import hmm.architecturestudio.management.exception.PrivilegesException;
+import hmm.architecturestudio.management.model.Customer;
+import hmm.architecturestudio.management.repository.CustomersRepository;
+import hmm.architecturestudio.management.util.PrivilegesChecker;
+
+@Service
+public class CustomersService {
+	
+    @Autowired
+    private CustomersRepository customersRepository;
+
+    @Autowired
+    private PrivilegesChecker privilegesChecker;
+    
+    public List<Customer> findAll() throws PrivilegesException {
+        if (!privilegesChecker.hasPrivilege("READ_CUSTOMERS",
+                SecurityContextHolder.getContext().getAuthentication().getAuthorities())
+        )
+        {
+            throw new PrivilegesException("READ_CUSTOMERS");
+        }
+
+        return this.customersRepository.findAll();
+    }
+
+}
