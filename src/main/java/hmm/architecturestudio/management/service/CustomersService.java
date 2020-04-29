578730b09b394ec0ad515e768adc667b0afde54d
hermoz <herminiamozomoreno@gmail.com>
2020-04-29 19:21:00 +0200

    CustomersService - findById

--- src/main/java/hmm/architecturestudio/management/service/CustomersService.java
+++ src/main/java/hmm/architecturestudio/management/service/CustomersService.java
@@ -1,6 +1,7 @@
 package hmm.architecturestudio.management.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -29,6 +30,18 @@ public class CustomersService {
         }
 
         return this.customersRepository.findAll();
+    }
+    
+    public Optional<Customer> findById(Long id) throws PrivilegesException {
+
+        if (!privilegesChecker.hasPrivilege("READ_CUSTOMERS",
+                SecurityContextHolder.getContext().getAuthentication().getAuthorities())
+        )
+        {
+            throw new PrivilegesException("READ_CUSTOMERS");
+        }
+
+        return this.customersRepository.findById(id);
     }
 
 }
