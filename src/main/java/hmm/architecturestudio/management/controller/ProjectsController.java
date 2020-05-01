6a74555f69ab25d73c5e883dbc5a96eedb9b0056
hermoz <herminiamozomoreno@gmail.com>
2020-05-01 15:53:23 +0200

    ProjectsController - deleteProject endpoint

--- src/main/java/hmm/architecturestudio/management/controller/ProjectsController.java
+++ src/main/java/hmm/architecturestudio/management/controller/ProjectsController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -98,6 +99,16 @@ public class ProjectsController {
 	    Project updatedProject = projectsService.updateProject(project);
 	
 	    return convertProjectToDto(updatedProject);
+	}
+    
+    /*
+     * Delete User
+     */
+    
+    @DeleteMapping(value = "/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteProject(@PathVariable("id") Long id) throws Exception {
+    	projectsService.deleteById(id);
 	}
     
     /*
