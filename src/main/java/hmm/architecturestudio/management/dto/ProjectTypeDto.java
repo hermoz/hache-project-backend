c170dfd1a142e3edca10541bba9a4fdc9688dc15
hermoz <herminiamozomoreno@gmail.com>
2020-05-01 14:45:32 +0200

    ProjectType creación de DTO

---
+++ src/main/java/hmm/architecturestudio/management/dto/ProjectTypeDto.java
@@ -0,0 +1,27 @@
+package hmm.architecturestudio.management.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class ProjectTypeDto {
+
+    @NotBlank
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
