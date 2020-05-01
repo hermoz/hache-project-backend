6363b642c776b5e694c522144d0e0bb1eb104e9c
hermoz <herminiamozomoreno@gmail.com>
2020-05-01 15:24:00 +0200

    ProjectsRepository - query búsqueda por título findByTitle

--- src/main/java/hmm/architecturestudio/management/repository/ProjectsRepository.java
+++ src/main/java/hmm/architecturestudio/management/repository/ProjectsRepository.java
@@ -1,9 +1,15 @@
 package hmm.architecturestudio.management.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import hmm.architecturestudio.management.model.Project;
 
 public interface ProjectsRepository extends JpaRepository<Project, Long> {
 
+	 @Query("SELECT p FROM Project p WHERE p.title = :title")
+	    public Optional<Project> findByTitle(@Param("title") String title);
 }
